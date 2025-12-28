50c2845c39bed70cf8cff123f872a31327999a29
sofiagazolla <sofgazolla@gmail.com>
2025-12-28 16:52:58 -0300

    feat: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Sobre from './pages/Sobre/Sobre'
+import Depoimentos from './pages/Depoimentos/Depoimentos'
+import Contato from './pages/Contato/Contato'
 
 function App() {
   return (
-    <div>
-      Hello World!
+    <div className='App'>
+        <Navbar />
+
+        <Routes>
+
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<Sobre />} />
+            <Route path="/statements" element={<Depoimentos />} />
+            <Route path="/contact" element={<Contato />} />
+
+        </Routes>
+
     </div>
   )
 }
