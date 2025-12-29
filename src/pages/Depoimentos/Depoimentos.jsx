8eef44df07c0887790d0417ea50f1efe391d7064
sofiagazolla <sofgazolla@gmail.com>
2025-12-28 21:36:18 -0300

    feat: card de depoimentos

--- src/pages/Depoimentos/Depoimentos.jsx
+++ src/pages/Depoimentos/Depoimentos.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import CardStatements from '../../components/CardStatements/CardStatements'
 
 function Depoimentos() {
   return (
-    <div>
-      Depoimentos
-    </div>
+    
+    <CardStatements
+        review="Excelente floricultura! Precisei de um arranjo de última hora e eles montaram ele muito rápido e de maneira lindíssima. Cuidado nota 10, atendimento ao cliente nota 10 e muito profissionalismo. Recomendo de olhos fechados"
+        imgSrc="/clientes/1.png"
+        imgAlt="lalala"
+        name="Sofia Gazolla"
+        date="5 de Novembro de 2025"
+    />
+
   )
 }
 
