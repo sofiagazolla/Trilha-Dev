0bd3479c0b308b276d8cb7ec0110836259fae18c
sofiagazolla <sofgazolla@gmail.com>
2026-01-12 14:51:57 -0300

    refactor: componentização do carousel

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -10,58 +10,26 @@ import 'swiper/css';
 import services from './ServicesData.js';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import { PiArrowCircleRightFill, PiArrowCircleLeftFill } from 'react-icons/pi';
+import CarouselSwiper from '../CarouselSwiper/CarouselSwiper.jsx';
 
 function Services() {
 	return (
 		<div className='container-servicos'>
 			<h1> Nossos Serviços </h1>
 
-			<div className='carousel-services'>
-				<button className='left-arrow'>
-					<FaChevronLeft size={24} />
-				</button>
+            <CarouselSwiper>
+                {services.map((service, index) => (
+                    <SwiperSlide key={index}>
+                        <CardServices
+                            imgSrc={service.imgSrc}
+                            imgAlt={service.imgAlt}
+                            bgColor={service.bgColor}
+                            subtitle={service.subtitle}
+                        />
+                    </SwiperSlide>
+                ))}
+            </CarouselSwiper>
 
-				<button className='right-arrow'>
-					<FaChevronRight size={24} />
-				</button>
-
-				<Swiper
-					modules={[Navigation]}
-					spaceBetween={20}
-					slidesPerView={3}
-					loop={true}
-					speed={500}
-					navigation={{
-						prevEl: '.left-arrow',
-						nextEl: '.right-arrow',
-					}}
-					breakpoints={{
-						0: {
-							slidesPerView: 1,
-							spaceBetween: 10,
-						},
-						768: {
-							slidesPerView: 2,
-							spaceBetween: 15,
-						},
-						1024: {
-							slidesPerView: 3,
-							spaceBetween: 20,
-						},
-					}}
-				>
-					{services.map((service, index) => (
-						<SwiperSlide key={index}>
-							<CardServices
-								imgSrc={service.imgSrc}
-								imgAlt={service.imgAlt}
-								bgColor={service.bgColor}
-								subtitle={service.subtitle}
-							/>
-						</SwiperSlide>
-					))}
-				</Swiper>
-			</div>
 		</div>
 	);
 }
